01eb0e69a08a387d3fecee35ac47d29ebf2600cc
webju <emmanuel.walubengo@strathmore.edu>
2023-02-27 15:41:48 +0300

    first commit

---
+++ app/src/main/java/com/example/aqm/miscellaneous/PrivacyPolicyActivity.java
@@ -0,0 +1,16 @@
+package com.example.aqm.miscellaneous;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aqm.R;
+
+public class PrivacyPolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+    }
+}
